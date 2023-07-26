36ae6a537a8394dfd1e69605dd9f9576eeee32a0
cryptocode <cryptocode@zolo.io>
2023-07-26 22:44:04 +0200

    Make linenoise compile on more *nix systems

--- build.zig
+++ build.zig
@@ -43,7 +43,7 @@ pub fn build(b: *std.build.Builder) void {
     exe.linkLibrary(gc);
 
     if (!target.isWindows()) {
-        exe.addCSourceFile("deps/linenoise.c", &[_][]const u8{"-std=c99"});
+        exe.addCSourceFile("deps/linenoise.c", &[_][]const u8{"-std=c99", "-Wno-everything"});
         exe.addIncludePath("deps");
     }
 
